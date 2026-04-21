f178110e3f9752637ea0f3a533311b0e43bad9c8
Gabriel <53336672+NiGabriel@users.noreply.github.com>
2026-04-22 01:16:45 +0200

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig(({ mode }) => {
   loadEnv(mode, process.cwd(), '')
   return {
-    base: '/queryme/',
+    base: '/',
     plugins: [react()],
   }
 })
